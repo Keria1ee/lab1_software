8a9463aaebaddb41d76234af48338e3a4e611494
Keria1ee <770837792@qq.com>
2024-06-09 17:26:57 +0800

    黑盒

--- src/com/src/MainTest.java
+++ src/com/src/MainTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+
+    //by chinhitori
     private TextToGraph textToGraph;
     @BeforeEach
     void setUp() {
